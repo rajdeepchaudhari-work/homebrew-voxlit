0d595281e96fd71b9abee3035cbafa9e6c1d90e6
Rajdeep Chaudhari <rajdeepchaudhari@voxlit>
2026-04-07 13:16:18 +0100

    chore: bump voxlit to 1.0.1 (clean build)

--- Casks/voxlit.rb
+++ Casks/voxlit.rb
@@ -1,6 +1,6 @@
 cask "voxlit" do
   version "1.0.1"
-  sha256 "9d772caef28fbb3147237fb26a60a7bcc564d5347c0a148de272f760277b0e02"
+  sha256 "eec557eacc225fdb3c0699e61a2625857119d0635513d40c88f51bf69706cfdf"
 
   url "https://github.com/rajdeepchaudhari-work/voxlit/releases/download/v#{version}/voxlit-#{version}-arm64.dmg"
   name "Voxlit"
